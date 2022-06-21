9260efb5928ef5e15bc5af97a7e9fc5a5d7e02eb
jaku14 <jameskuhn91@gmail.com>
2022-06-21 18:43:04 -0400

    adding fizzbuzz solution for practice

---
+++ RandomProject/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number");
+int input = int.Parse(Console.ReadLine());
+
+for (int i = 1 ; i <= input; i++){
+
+    if (i % 3 == 0 && i % 5 == 0 ){
+        Console.WriteLine("fizzbuzz");
+    }
+    else if (i % 3 == 0){
+        Console.WriteLine("fizz");
+    }
+    else if (i % 5 == 0 ){
+        Console.WriteLine("buzz");
+    }
+
+    else{
+        Console.WriteLine(i);
+    }
+
+}
+
+
+
+
